af4bf989ef1af34e82c74cb470fc172d4e998028
Jacquin.Moon <jacquindev@outlook.com>
2024-10-14 23:47:50 +0700

    use source code archive for faster installation

--- scripts/wsl2-kernel/setup.sh
+++ scripts/wsl2-kernel/setup.sh
@@ -8,10 +8,14 @@ if [[ ! "$(uname -r)" =~ "WSL" ]]; then
     return
 fi
 
-check_apt_packages bc bison cpio dwarves flex libssl-dev libelf-dev python3 pahole
+check_apt_packages bc bison cpio dwarves flex libssl-dev libelf-dev python3 pahole wget
 
-git clone https://github.com/microsoft/WSL2-Linux-Kernel.git --depth 1 -b linux-msft-wsl-6.6.y
-builtin cd WSL2-Linux-Kernel
+KERNEL_VERSION=$(get_latest_release "microsoft/WSL2-Linux-Kernel")
+wget --show-progress --timestamping https://github.com/microsoft/WSL2-Linux-Kernel/archive/refs/tags/${KERNEL_VERSION}.tar.gz
+mkdir WSL2-Linux-Kernel
+tar -xvzf WSL2-Linux-Kernel-${KERNEL_VERSION}.tar.gz -C WSL2-Linux-Kernel
+rm -f WSL2-Linux-Kernel-${KERNEL_VERSION}.tar.gz
+builtin cd WSL2-Linux-Kernel && make -j$(nproc) KCONFIG_CONFIG=Microsoft/config-wsl
 
 # (Optional)
 # make menuconfig KCONFIG_CONFIG=Microsoft/config-wsl
@@ -22,13 +26,13 @@ make -j$(nproc) KCONFIG_CONFIG=Microsoft/config-wsl
 sudo make modules_install headers_install
 
 printf "Input a Kernel location (eg: /mnt/c/) " && read -r location
-cp arch/x86_64/boot/bzImage $location
+cp arch/x86/boot/bzImage $location
 cd .. && rm -rf WSL2-Linux-Kernel
 
 echo
-echo "Please edit your .wslconfig with your kernel location: $location"
-echo "For example, if your location is /mnt/c/ then your config would be:"
-echo "[WSL2]"
+echo 'Please edit your .wslconfig with your kernel location: $location'
+echo 'For example, if your location is /mnt/c/ then your config would be:'
+echo '[WSL2]'
 echo 'kernel=C:\\\\bzImage'
 echo
 
