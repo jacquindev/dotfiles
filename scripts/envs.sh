d28edeed300d494b7b9ef11b96753afdc9d62a11
Jacquin Moon <jacquindev@outlook.com>
2024-11-19 21:00:01 +0700

    Add files via upload

---
+++ scripts/envs.sh
@@ -0,0 +1,32 @@
+# xdg
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_BIN_HOME="$HOME/.local/bin"
+export XDG_RUNTIME_DIR="$HOME/.xdg"
+export XDG_PROJECTS_DIR="$HOME/Projects"
+
+# zsh
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+
+# ripgrep
+export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
+
+# nvm
+export NVM_DIR="$HOME/Code/nvm"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export npm_config_cache="$XDG_CACHE_HOME/npm"
+export PNPM_HOME="$XDG_DATA_HOME/pnpm"
+export YARN_CACHE_FOLDER="$XDG_CACHE_HOME/npm"
+
+# python
+export PYENV_ROOT="$HOME/Code/pyenv"
+
+# rust
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export CARGO_HOME="$HOME/Code/cargo"
+
+# go
+export GOROOT="$HOME/.local/share/go"
+export GOPATH="$HOME/Code/go"
---
+++ scripts/paths.sh
@@ -0,0 +1,45 @@
+# Function to add to path
+# References: - https://superuser.com/questions/39751/add-directory-to-path-if-its-not-already-there
+
+pathappend() {
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    PATH="${PATH:+"$PATH:"}$1"
+  fi
+}
+
+pathprepend() {
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    PATH="$1${PATH:+":$PATH"}"
+  fi
+}
+
+# Main bin
+[ -d "$HOME/bin" ] && pathprepend "$HOME/bin"
+[ -d "$HOME/.local/bin" ] && pathprepend "$HOME/.local/bin"
+[ -d /usr/local/sbin ] && pathprepend /usr/local/sbin
+
+# If you're using WSL and you have set /etc/wsl.conf [interop] as
+# appendWindowsPath=false, then you can add the path of the program you want
+# to use on Windows inside your WSL terminal here:
+#
+if uname -r | grep -i -q 'Microsoft'; then
+  if command -v wslvar >/dev/null 2>&1; then
+    USERPROFILE=$(wslpath "$(wslvar USERPROFILE)")
+    pathprepend "$USERPROFILE/AppData/Local/Programs/Microsoft VS Code/bin"
+  fi
+fi
+
+#DEV TOOLS #############################################################################
+# npm
+if command -v npm >/dev/null 2>&1; then pathappend "$(npm config get prefix)/bin"; fi
+if command -v pnpm >/dev/null 2>&1; then pathappend "$(pnpm config get prefix)"; fi
+if command -v yarn >/dev/null 2>&1; then pathappend "$(yarn global bin)"; fi
+
+# python (pyenv/poetry)
+[ -d "$PYENV_ROOT/bin" ] && pathprepend "$PYENV_ROOT/bin"
+[ -d "$PYENV_ROOT/shims" ] && pathprepend "$PYENV_ROOT/shims"
+[ -d "$PYENV_ROOT/versions/global/bin" ] && pathprepend "$PYENV_ROOT/versions/global/bin"
+[ -d "$POETRY_HOME/bin" ] && pathprepend "$POETRY_HOME/bin"
+
+# g
+[ -d "$GOPATH/bin" ] && pathprepend "$GOPATH/bin"
