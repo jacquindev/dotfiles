6a64a8bd021291af47d10fc4f05b5d5ab135f888
Jacquin Moon <jacquindev@outlook.com>
2025-03-20 07:08:51 +0700

    :hammer: chore: update vagrant configuration for zsh shell

--- .config/zsh/lib/vagrant.zsh
+++ .config/zsh/lib/vagrant.zsh
@@ -2,9 +2,5 @@
 
 if uname -r | grep -q microsoft; then
 	export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-
-	if command -v wslvar >/dev/null 2>&1; then
-		vagrant_windows_path=$(wslpath "$(wslvar USERPROFILE)")
-		export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="${vagrant_windows_path}/"
-	fi
+	export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/vagrant_home/"
 fi
