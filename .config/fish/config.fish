d3876d4f0c9dc7104c14ebab5955fb0ccc01b9ff
Jacquin Moon <jacquindev@outlook.com>
2025-03-20 07:10:46 +0700

    :bug: fix: change localation of Vagrant configuration in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,31 @@ if type -q bass
     bass ". $HOME/.config/path_add.sh"
 end
 
+# wsl machine
+if type -q wslvar
+    set windows_user_home (wslpath (wslvar USERPROFILE))
+    # VS Code
+    if not type -q code
+        if test -d "$windows_user_home/AppData/Local/Programs/Microsoft VS Code/bin"
+            set -gx --prepend PATH "$windows_user_home/AppData/Local/Programs/Microsoft VS Code/bin"
+        end
+    end
+
+    # virtualbox
+    set windows_program_files (wslpath (wslvar ProgramW6432))
+    if test -d "$windows_program_files/Oracle/VirtualBox"
+        set -gx --prepend PATH "$windows_program_files/Oracle/VirtualBox"
+    end
+
+    # vagrant
+    if type -q vagrant
+        set -gx VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1
+        set -gx VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH /mnt/c/vagrant_home/
+    end
+
+    set --erase windows_user_home windows_program_files
+end
+
 # kubectl aliases
 # - https://github.com/ahmetb/kubectl-aliases
 test -f ~/.config/fish/.kubectl_aliases.fish && source ~/.config/fish/.kubectl_aliases.fish
