d39ea48ecbc31890610c6cf0e2791bb9c25bf695
Jacquin Moon <jacquindev@outlook.com>
2025-03-23 12:45:34 +0700

    :gear: config(config): update `neovim` configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
+-- loader
+if vim.loader then
+  vim.loader.enable()
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,10 +12,10 @@ g.autoformat = true
 g.editorconfig = true
 
 -- Disable builtin providers
--- g.loaded_python3_provider = 0
--- g.loaded_perl_provider = 0
--- g.loaded_ruby_provider = 0
--- g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
 
 -- Python settings
 g.lazyvim_python_lsp = "basedpyright"
