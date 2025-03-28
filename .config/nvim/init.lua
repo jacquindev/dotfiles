b0a9d25991cf2e8d91253c70cd0b07037cbf71e8
Jacquin Moon <jacquindev@outlook.com>
2025-03-28 12:36:08 +0700

    :sparkles: feat(nvim): add `mise` integrations for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,6 @@ end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
